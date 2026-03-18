89f388b820d86053239d04e8b67f3256162f66f8
balasubramaniyand2203 <balasubramaniyand2203@gmail.com>
2026-03-18 14:31:20 +0530

    Update output.tf

--- acr/output.tf
+++ acr/output.tf
@@ -6,3 +6,12 @@ output "acr_id" {
   value = azurerm_container_registry.acr.id
 }
 
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
