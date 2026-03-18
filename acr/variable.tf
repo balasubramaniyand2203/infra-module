e5dece1481034a2b639602b90abd1d52f8b8cfab
balasubramaniyand2203 <balasubramaniyand2203@gmail.com>
2026-03-18 12:39:24 +0530

    Update variable.tf

--- acr/variable.tf
+++ acr/variable.tf
@@ -12,9 +12,17 @@ variable "acr_name" {
 }
 
 variable "acr_sku" {
-  type = string
+  type    = string
+  default = "Basic"
 }
 
 variable "acr_admin_enabled" {
-  type = bool
+  type    = bool
+  default = false
+}
+
+# --- ADD THIS BLOCK TO FIX THE ERROR ---
+variable "aks_principal_id" {
+  description = "The Principal ID of the AKS Kubelet Identity to allow image pulls"
+  type        = string
 }
