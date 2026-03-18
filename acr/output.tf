faf1afc055e40246c2ffda661806ed53660effb5
balasubramaniyand2203 <balasubramaniyand2203@gmail.com>
2026-03-18 12:49:11 +0530

    Update output.tf

--- acr/output.tf
+++ acr/output.tf
@@ -1,15 +1,7 @@
-output "aks_cluster_name" {
-  description = "The name of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.name
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
 }
 
-output "aks_cluster_id" {
-  description = "The ID of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.id
-}
-
-# --- CRITICAL: This is the ID needed for the ACR Role Assignment ---
-output "kubelet_identity_id" {
-  description = "The Object ID of the user-assigned identity used by the AKS Kubelet"
-  value       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+output "acr_id" {
+  value = azurerm_container_registry.acr.id
 }
