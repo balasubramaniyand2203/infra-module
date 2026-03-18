4b6d91cf0896741cd37f566f27ab868295c590d3
balasubramaniyand2203 <balasubramaniyand2203@gmail.com>
2026-03-18 12:55:59 +0530

    Update output.tf

--- acr/output.tf
+++ acr/output.tf
@@ -5,8 +5,4 @@ output "acr_login_server" {
 output "acr_id" {
   value = azurerm_container_registry.acr.id
 }
-output "kubelet_identity_id" {
-  description = "The Object ID of the user-assigned identity used by the AKS Kubelet"
-  # Replace 'aks' with the actual name from your resource block
-  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
+
