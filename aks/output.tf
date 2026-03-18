f7c4a09232d7011903eeb2199193412ac4125bd2
balasubramaniyand2203 <balasubramaniyand2203@gmail.com>
2026-03-18 14:27:25 +0530

    Update output.tf

--- aks/output.tf
+++ aks/output.tf
@@ -1,14 +1,13 @@
+# modules/aks/output.tf
 
-  output "kube_config" {
-    value = azurerm_kubernetes_cluster.aks_cluster.kube_config
-    sensitive = true  
-  }
-
-output "used_subnet_id" {
-  value = var.vnet_subnet_id # ✅ This uses the variable defined in AKS
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config
+  sensitive = true  
 }
 
-# modules/aks/output.tf
+output "used_subnet_id" {
+  value = var.vnet_subnet_id 
+}
 
 output "aks_cluster_name" {
   description = "The name of the AKS cluster"
@@ -20,6 +19,8 @@ output "aks_cluster_location" {
   value       = azurerm_kubernetes_cluster.aks_cluster.location
 }
 
+# FIXED BLOCK BELOW:
 output "kubelet_identity_id" {
-  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  # Changed .aks. to .aks_cluster. to match your main.tf
+  value = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
 }
